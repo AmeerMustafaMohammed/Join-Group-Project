e50492ed6c6af7306bd9ce141136da0284a49f24
Ameer Mohammed <ameer.coding1994@gmail.com>
2022-03-31 00:40:51 +0200

    creating neu categories

--- js/addTask.js
+++ js/addTask.js
@@ -2,6 +2,7 @@ let currentGroup;
 currentGroup = "DEMO";
 let tasks = [];
 let allUser = [];
+let allCategories = [];
 
 function init() {
     if (loadJSON('TASKS')) {
@@ -10,54 +11,14 @@ function init() {
     if (loadJSON('allUser')) {
         allUser = loadJSON('allUser');
     }
-    showAllUsers();
-    loadAllUser();
     loadGroupName(); //name of registered group is loaded from login.js
-}
 
-/*  USERS */
-
-function loadAllUser() {
-    if (loadJSON('allUser')) {
-        allUser = loadJSON('allUser')
-    }
+    loadAllUser(); // Load all user from LocalStorage
+    showAllUsers(); // Show updated User on Screen
+    loadCategories();
+    showallCategories();
 
 }
-
-function showAllUsers() {
-    let memberOptions = document.getElementById('asign-member');
-    memberOptions.innerHTML = '';
-    if (!allUser.length == 0) {
-        for (let i = 0; i < allUser.length; i++) {
-            if (allUser[0]['gruppe'] == currentGroup) {
-                memberOptions.innerHTML += `
-                <option value="max musterman">${allUser[i]['name']}</option>
-                `;
-            }
-        }
-    } else {
-        memberOptions.innerHTML = `
-        <option value="keine User">Add USER</option>
-        `;
-    }
-
-}
-
-
-
-function addNeuUser() {
-    let neuUser = document.getElementById('neu-user').value;
-    let user = {
-        name: neuUser,
-        gruppe: currentGroup
-    }
-
-    allUser.push(user)
-    saveJson('allUser', allUser);
-    showAllUsers();
-}
-
-
 
 /* ADDING TASKS *********************** */
 function catchInputs() {
@@ -94,6 +55,84 @@ function addTaskToTasks(neuTitle, neuCategory, neuDescription, neuDate, urgency,
     saveJson('TASKS', tasks)
 }
 
+
+
+/* CUSTOMIZING USER AND CATEGORIES */
+
+/*  USERS */
+function loadAllUser() {
+    if (loadJSON('allUser')) {
+        allUser = loadJSON('allUser')
+    }
+
+}
+
+function showAllUsers() {
+    let memberOptions = document.getElementById('asign-member');
+    memberOptions.innerHTML = '';
+    if (!allUser.length == 0) {
+        for (let i = 0; i < allUser.length; i++) {
+            if (allUser[0]['gruppe'] == currentGroup) {
+                memberOptions.innerHTML += `
+                <option value="max musterman">${allUser[i]['name']}</option>
+                `;
+            }
+        }
+    } else {
+        memberOptions.innerHTML = `
+        <option value="keine User">Add USER</option>
+        `;
+    }
+
+}
+
+function addNeuUser() {
+    let neuUser = document.getElementById('neu-user').value;
+    let user = {
+        name: neuUser,
+        gruppe: currentGroup
+    }
+
+    allUser.push(user)
+    saveJson('allUser', allUser);
+    showAllUsers();
+}
+
+/* CATEGORYIES */
+function loadCategories() {
+    if (loadJSON('allCategories')) {
+        allCategories = loadJSON('allCategories')
+    }
+}
+
+function showallCategories() {
+    let categoryOptions = document.getElementById('add-category');
+    if (!allCategories.length == 0) {
+        for (let i = 0; i < allCategories.length; i++) {
+            if (allCategories[0]['gruppe'] == currentGroup) {
+                categoryOptions.innerHTML += `
+                <option value="category">${allCategories[i]['name']}</option>
+                `;
+            }
+        }
+    }
+}
+
+function addNeuCategory() {
+    let neuCategory = document.getElementById('neu-Category').value;
+    let Category = {
+        name: neuCategory,
+        gruppe: currentGroup
+    }
+
+    allCategories.push(Category)
+    saveJson('allCategories', allCategories);
+    showallCategories();
+}
+
+function editCategory() {
+
+}
 /* JSON */
 
 function saveJson(key, Json) {
