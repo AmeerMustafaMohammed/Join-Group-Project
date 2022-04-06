8053ba006e2e937ce3dc2c0452d28bd98166807b
Tobias Matthies <tbsmatthies@gmail.com>
2022-04-06 11:15:24 -0400

    reworked responsivness of navigation

--- js/navigation.js
+++ js/navigation.js
@@ -1,13 +1,32 @@
 let closeMenu = true;
+let openMenuButton = '/img/navigation/open.svg';
+let closeMenuButton = '/img/navigation/close.svg';
+
+
+function initNav() {
+    addSource();
+}
+
+
+function addSource() {
+    if (closeMenu) {
+        document.getElementById('close-btn').src = openMenuButton;
+    } else {
+        document.getElementById('close-btn').src = closeMenuButton;
+    }
+}
+
 
 function openCloseNavigation() {
     let navigation = document.getElementById("navigation");
     if (closeMenu) {
         navigation.classList.add("open-menu")
         closeMenu = false;
+        addSource();
     } else {
         navigation.classList.remove("open-menu")
         closeMenu = true;
+        addSource();
     }
 
 }
