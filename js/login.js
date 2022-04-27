12030f4cf1336e41b7d339ae200964e1a5c6189e
Tobias Matthies <tbsmatthies@gmail.com>
2022-04-27 13:02:38 -0400

    fixed user login bug, added jsdoc comments to login.js

--- js/login.js
+++ js/login.js
@@ -7,7 +7,7 @@ let user; //username
 let currentUser = []; //combination of group and user
 
 
-async function initLogin() {
+/*async function initLogin() {
     await downloadFromServer();
     loadItems();
 }
@@ -18,19 +18,32 @@ function loadGroupName() {
     if (currentUser) {
         document.getElementById('group_name_add_tasks').innerHTML = currentUser['group'];
     }
-}
+}*/
 
 
+/**
+ * 
+ * this function is used to show the element with the id of the parameter "id"
+ * @param {string} id 
+ */
 function show(id) {
     document.getElementById(id).classList.remove('d-none');
 }
 
-
+/**
+ * 
+ *  this function is used to hide the element with the id of the parameter "id"
+ * @param {string} id 
+ */
 function hide(id) {
     document.getElementById(id).classList.add('d-none');
 }
 
 
+/**
+ * 
+ * this function checks if the requested group exists. If it does it leads the user to the login, if not it shows a message to the user
+ */
 function checkIfGroupExists() {
     let group = document.getElementById('group_name_input').value;
 
@@ -43,7 +56,11 @@ function checkIfGroupExists() {
     }
 }
 
-
+/**
+ * 
+ * this function shows the login card
+ * @param {string} group 
+ */
 function goToLogin(group) {
     requestedGroup = group;
     hide('group_search');
@@ -51,13 +68,20 @@ function goToLogin(group) {
     hideWarnigns();
 }
 
-
+/**
+ * 
+ * this function is used to hide the warnigs shown, when a group does not exist or the user did not type anything
+ */
 function hideWarnigns() {
     document.getElementById('group_not_found').classList.add('d-none');
     document.getElementById('empty_group_input').classList.add('d-none');
 }
 
 
+/**
+ * 
+ * this function checks, if the user exists. If the user exists, it sets the user and leads him to the landing page. If not, it shows a warning
+ */
 function userLogIn() {
     let username = document.getElementById('username_input').value;
 
@@ -65,10 +89,19 @@ function userLogIn() {
         user = username;
 
         setUser();
+        window.location.href = 'addTasks.html'
+    } else if (users.indexOf(username) == -1) {
+        showWarningUserNotFound();
+    } else {
+        showWarningEmptyInput();
     }
 }
 
 
+/**
+ * 
+ * this function is used to create a new user
+ */
 function createAccount() {
     let username = document.getElementById('create_username_input').value;
 
@@ -80,6 +113,10 @@ function createAccount() {
 }
 
 
+/**
+ * 
+ * this function is used to set the current combination of user and group
+ */
 function setUser() {
     currentUser = {
         'group': requestedGroup,
@@ -90,18 +127,40 @@ function setUser() {
 }
 
 
+/**
+ * 
+ * this function shows the warning for a wrong username and hides the other warning in case it is displayed
+ */
+function showWarningUserNotFound() {
+    document.getElementById('empty_group_input').classList.add('d-none');
+    document.getElementById('user_not_found').classList.remove('d-none');
+}
+
+
+/**
+ * 
+ * this function shows the warning for an empty input and hides the other warning in case it is displayed
+ */
 function showWarningEmptyInput() {
     document.getElementById('empty_group_input').classList.remove('d-none');
     document.getElementById('group_not_found').classList.add('d-none');
 }
 
 
+/**
+ * 
+ * this function shows the warning for a wrong group input and hides the other warning in case it is displayed
+ */
 function showWarningGroupNotExisting() {
     document.getElementById('group_not_found').classList.remove('d-none');
     document.getElementById('empty_group_input').classList.add('d-none');
 }
 
 
+/**
+ * 
+ * this function is used to create and then set a new group
+ */
 function addGroup() {
     let groupName = document.getElementById('found_group_name_input').value;
 
@@ -113,6 +172,10 @@ function addGroup() {
 }
 
 
+/**
+ * 
+ * this function is used to save items into the local storage
+ */
 function saveItems() {
     localStorage.setItem('groups', JSON.stringify(groups));//needs backend saving
     localStorage.setItem('users', JSON.stringify(users));//needs backend saving
@@ -123,7 +186,11 @@ function saveItems() {
 }
 
 
-async function loadItems() {
+/**
+ * 
+ * this function is used to load all the needed items
+ */
+function loadItems() {
     let savedUser = localStorage.getItem('currentUser');
 
     let groupsAsString =  localStorage.getItem('groups'); //groupsAsString
